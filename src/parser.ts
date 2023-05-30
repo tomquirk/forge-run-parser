eb3f62c04123c5d7bb3974f9471ff128e41967ad
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-05-30 11:37:53 +0930

    runnit

---
+++ src/parser.ts
@@ -0,0 +1,23 @@
+type ContractAddress = `0x${string}`;
+
+export interface ForgeDeploy {
+  receipts: { contractAddress: ContractAddress }[];
+  transactions: { contractName: string; contractAddress: ContractAddress }[];
+}
+
+/**
+ * Return the address of a given contract from a Forge deploy.
+ * @param contractName Name of the contract to get the address for.
+ * @param forgeDeploy The Forge deployment, typically located in `broadcast/<contract-name>.sol/<chain-id>/run-latest.json
+ * @returns The address of the contract.
+ */
+export function addressFor(
+  contractName: string,
+  forgeDeploy: ForgeDeploy
+): string | undefined {
+  const contractAddress = forgeDeploy.transactions.find(
+    (tx) => tx.contractName === contractName
+  )?.contractAddress;
+
+  return contractAddress;
+}
